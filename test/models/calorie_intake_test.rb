e15a8d65d41b4daa7054e51fa9e800aeea73162f
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-04 14:41:38 -0400

    Add calories intake methods calorie total and date

--- test/models/calorie_intake_test.rb
+++ test/models/calorie_intake_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class CalorieIntakeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "total calories" do
+    assert_equal 3500, calorie_intakes(:one).calorie_total
+  end
+
+  test "input date" do
+    assert_equal "2015-06-11".to_date, calorie_intakes(:two).input_date
+  end
 end
