6fe04b15895c22c015b88f749deb51ecf599e4c0
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-04 15:53:13 -0400

    Add daily steps and calories to models and tests

--- app/models/pedometer_reading.rb
+++ app/models/pedometer_reading.rb
@@ -6,4 +6,14 @@ class PedometerReading < ActiveRecord::Base
   def miles
     self.steps/2000
   end
+
+  def self.daily_steps
+    total = 0
+    self.select do |e|
+      if e.date.day == Time.now.day && e.date.month == Time.now.month && e.date.year == Time.now.year
+        total += e.steps
+      end
+    end
+    total
+  end
 end
