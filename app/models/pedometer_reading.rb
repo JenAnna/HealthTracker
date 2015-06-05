8951dd65d0bd07bf0d3f1a81ef5b1994fba86561
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-05 12:08:24 -0400

    Modify views and edit pedo model

--- app/models/pedometer_reading.rb
+++ app/models/pedometer_reading.rb
@@ -1,9 +1,15 @@
 class PedometerReading < ActiveRecord::Base
   validates :date, presence: true
   validates :steps, presence: true
+  before_save :calories_burned
 
   def calories
-    self.steps/20
+    self.calories_burned = self.steps/20
+  end
+
+  def self.daily_step_calories
+    calories_step = self.select {|e| e.date == Date.today}
+    calories_step.sum &:calories_burned
   end
 
   def miles
