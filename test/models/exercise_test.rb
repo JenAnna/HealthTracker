414458a3fa69c84e8936833babaff2457c2a3284
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-05 07:41:28 -0400

    Excerise daily method getting nil

--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -2,9 +2,17 @@ require 'test_helper'
 
 class ExerciseTest < ActiveSupport::TestCase
 
-  test "calculate_calories_burned" do
+  test "calories burned" do
     t = ExerciseType.create!(description: "fake", calories_burned_per_minute: 10)
     run = Exercise.create!(exercise_type_id: t.id, time_in_minutes: 30, description: "Running", date: Date.today)
     assert_equal 300, run.calculate_calories_burned
+  end
+
+  test "daily calorie burn" do
+    t = ExerciseType.create!(description: "fake", calories_burned_per_minute: 10)
+    run = Exercise.create!(exercise_type_id: t.id, time_in_minutes: 30, description: "Running", date: Date.today)
+    run2 = Exercise.create!(exercise_type_id: t.id, time_in_minutes: 30, description: "Running", date: Time.new(2014, 06, 01))
+    run3 = Exercise.create!(exercise_type_id: t.id, time_in_minutes: 45, description: "Running", date: Date.today)
+    assert_equal 400, Exercise.daily_calories_burned
   end
 end
