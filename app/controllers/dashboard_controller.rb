e810cfdc113ee4a8698e8de4ffabed24999f0eca
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-07 13:27:59 -0400

    pull

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
 
   def index
+    @calorie_intake = CalorieIntake.new
+   @exercise = Exercise.new
+   @pedometer_reading = PedometerReading.new
+   @weigh_in = WeighIn.new
     @calorie_intakes = CalorieIntake.where(params[:user_id]).all
     @pedometer_readings = PedometerReading.where(params[:user_id]).all
     @exercises = Exercise.where(params[:user_id]).all
